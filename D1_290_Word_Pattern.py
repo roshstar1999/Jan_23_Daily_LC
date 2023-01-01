3fc568dccebd95ee35d3127b5a4c4010e12f8074
N V Roshni <roshstar1999@gmail.com>
2023-01-01 06:39:44 +0530

    Update D1_290_Word_Pattern.py
    # Intuition
    Found the tricky part here as the given pattern wont be following any alphabetical order sequence, it could be random.
    
    # Approach
    Store the pattern sequence of the string s as well as the given pattern and then return if both same.
    
    # Complexity
    - Time complexity: O(n)
    
    - Space complexity:0(n)

--- D1_290_Word_Pattern.py
+++ D1_290_Word_Pattern.py
@@ -1,15 +1,18 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        #storing pattern for the string s
         res=''
-        d={}
+        d1={}
         alp='abcdefghijklmnopqrstuvwxyz'
         ptr=0
         for i in s.split():
-            if i not in d:
+            if i not in d1:
                 
-                d[i]=alp[ptr]
+                d1[i]=alp[ptr]
                 ptr+=1
-            res+=d[i]
+            res+=d1[i]
+        #storing pattern for the given pattern in same manner
+           
         res2=''
         ptr=0
         d2={}
@@ -19,4 +22,6 @@ class Solution:
                 d2[i]=alp[ptr]
                 ptr+=1
             res2+=d2[i]
+            
+        #comparing both
         return res==res2
