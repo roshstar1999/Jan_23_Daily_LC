016f6a059471431fa62f0aeb804b71776563144d
N V Roshni <roshstar1999@gmail.com>
2023-01-10 06:28:20 +0530

    Create D10_Same_Tree.py

---
+++ D10_Same_Tree.py
@@ -0,0 +1,18 @@
+# easy
+#     class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if (p and not q) or (not p and q):
+            return False
+        if not p and not q:
+            return True
+        
+        if p.val!=q.val:
+            return False
+        return self.isSameTree(p.left,q.left)==True and self.isSameTree(p.right,q.right)==True
+
+        return p.val==q.valisSameTree(p)==isSameTree(q)
